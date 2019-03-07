ba92a4b33ec1e5ecbafa930a564af58f93adc29d
juliano <juliano@julianos-iMac.local>
2019-03-07 20:20:25 -0300

    Tela de about e tela do jogo

---
+++ quiz/Model/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  quiz
+//
+//  Created by juliano on 07/03/19.
+//  Copyright © 2019 juliano. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    init(){
+        
+    }
+}
---
+++ quiz/Model/Quiz.swift
@@ -0,0 +1,25 @@
+//
+//  Quiz.swift
+//  quiz
+//
+//  Created by juliano on 07/03/19.
+//  Copyright © 2019 juliano. All rights reserved.
+//
+
+import Foundation
+
+class Quiz {
+    
+    
+    
+    init(){
+        
+    }
+    
+    func start (){
+        
+    }
+    
+    
+    
+}
